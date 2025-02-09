66cbdc3d07e7bd75b0543e5442a3f8dcaf100ad5
shwetas1210 <singh7172shweta@gmail.com>
2025-02-09 19:09:32 +0530

    designed home page

---
+++ src/components/Slide.jsx
@@ -0,0 +1,14 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className='slide'>
+      <h1>
+      Embrace the journey, escape the ordinary <br />
+      your perfect stay awaits!
+      </h1>
+    </div>
+  )
+}
+
+export default Slide
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar"
+import Slide from "../components/Slide"
+import Categories from "../components/Categories"
+
+function HomePage() {
+  return (
+    <>
+    <Navbar/>
+    <Slide/>
+    <Categories />
+    </>
+  )
+}
+
+export default HomePage
